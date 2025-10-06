643e9485c82e85a902316f4b42056854a53a60e9
Marcin <marcin.lew.slon@gmail.com>
2025-10-06 17:36:06 +0200

    collector track repeat limit collector pitch randomization

--- scripts/controllers/the_collector/fsm/state_collector_talking.gd
+++ scripts/controllers/the_collector/fsm/state_collector_talking.gd
@@ -3,8 +3,10 @@ class_name StateCollectorTalking
 
 
 @export var collector_audio_controller: CollectorAudioController
-@export var word_delay_sec: float = 0.2
+@export var word_delay_sec: float = 0.1
 @export var repeat_limit: int = 3
+@export var pitch_range_min: float = 0.9
+@export var pitch_range_max: float = 1.1
 
 var _recently_played: Array[int] = []
 
@@ -20,6 +22,7 @@ func exit(_fsm_context: FsmContext) -> void:
 func _start_next_sound() -> void:
 	await get_tree().create_timer(word_delay_sec).timeout
 	collector_audio_controller.stream = _get_random_track()
+	collector_audio_controller.pitch_scale = randf_range(pitch_range_min, pitch_range_max)
 	collector_audio_controller.play()
 	
 	
